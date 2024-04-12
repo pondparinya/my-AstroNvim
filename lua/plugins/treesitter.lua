e8239149a3601f3c5121c7900b35c7eda0c6d41b
pondparinya <pondparinyaaa@gmail.com>
2024-04-12 17:52:17 +0700

    set AstroNvim to owner

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -10,6 +10,9 @@ return {
     opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
       "lua",
       "vim",
+      "go",
+      "gomod",
+      "gosum",
       -- add more arguments for adding more treesitter parsers
     })
   end,
